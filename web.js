ad6a4501d52f2984a9f68c0793092a8a58676ccd
Rupert H <rupert.hochholzer@gmail.com>
2013-07-10 21:45:23 +0000

    updating web.js again

--- web.js
+++ web.js
@@ -5,11 +5,8 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-    var outstring;
     var buffer = new buffer();
-    outstring = buffer.toString('utc 8', fs.readFileSync("index.html")); 
-
-    response.send(outstring);
+    response.send(buffer.toString('utc 8', fs.readFileSync("index.html"))); 
 });
 
 
