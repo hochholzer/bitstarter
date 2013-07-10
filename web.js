96e4e98b7d93871c64d5537fa68f4457a177baa7
Rupert H <rupert.hochholzer@gmail.com>
2013-07-10 21:24:06 +0000

    updating web.js

--- web.js
+++ web.js
@@ -2,9 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+ var outstring;
+ fs.readFile('./index.html' function read(err, data) {
+     if (err) {
+	 throw err;
+     }
+     outstring = data;
+   }  
+ );
+   response.send(outstring);
+}
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
